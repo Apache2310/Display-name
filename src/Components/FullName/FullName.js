2ab3297b1a0c92178c77c67361b90d7347d60714
Siddhant Patwa <siddhantpatwa8@gmail.com>
2024-03-14 01:17:40 +0530

    initial commit

---
+++ src/Components/FullName/FullName.js
@@ -0,0 +1,47 @@
+import React, {useState} from "react";
+
+const FullName = () => {
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [fullName, setFullName] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(firstName && lastName){
+            const fullName = `${firstName} ${lastName}`;
+            setFullName(fullName)
+        }else{
+            alert("Please fill the mandatory fields");
+        }
+    };
+
+    return(
+        <div>
+            <h1>Full Name Display</h1>
+            <form onSubmit={handleSubmit}>
+                <label>
+                    First Name:
+                    <input
+                    type="text" 
+                    value={firstName}  
+                    onChange={(e) => setFirstName(e.target.value)}
+                    />
+                </label>
+                <br />
+                <label>
+                   Last Name:
+                   <input
+                     type="text"
+                     value={lastName}
+                     onChange={(e)=>setLastName(e.target.value)}
+                    />
+               </label>
+               <br />
+               <button  type='submit'>Submit</button>
+               {fullName && <h3>{fullName}</h3>}
+            </form>
+        </div>
+    );
+}
+export default FullName;
