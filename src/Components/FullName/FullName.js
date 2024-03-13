804c6057326e0f89d17fa53165fc8a06b1f22beb
Siddhant Patwa <siddhantpatwa8@gmail.com>
2024-03-14 01:51:15 +0530

    1st

--- src/Components/FullName/FullName.js
+++ src/Components/FullName/FullName.js
@@ -8,16 +8,14 @@ const FullName = () => {
     const handleSubmit = (e) => {
         e.preventDefault();
 
-        const regex = /^[a-zA-Z\s]*$/;
+        // const regex = /^[a-zA-Z\s]*$/;
         if(!firstName.trim( ) || !lastName.trim()) {
             alert("Please fill the mandatory fields.");
-        }else if(!regex.test(firstName) || !regex.test(lastName)){
-            alert("Invalid entry! Please enter only letters and spaces.")
+        // }else if(!regex.test(firstName) || !regex.test(lastName)){
+        //     alert("Invalid entry! Please enter only letters and spaces.")
         } else{
             const fullName = ` ${firstName} ${lastName}`; 
             setFullName(fullName);
-            setFirstName('');
-            setLastName('');
         }
     };
 
